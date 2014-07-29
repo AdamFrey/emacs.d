da20b9372661f1784581d96335d0a736c79ca51f
Adam Frey <adam@adamfrey.me>
2014-07-29 01:29:16 -0400

    Add spell rememberer

--- lisp/init-spelling.el
+++ lisp/init-spelling.el
@@ -3,4 +3,24 @@
 (when (executable-find ispell-program-name)
   (require 'init-flyspell))
 
+; http://endlessparentheses.com/ispell-and-abbrev-the-perfect-auto-correct.html
+(define-key ctl-x-map "\C-y" 'endless/ispell-word-then-abbrev)
+
+(defun endless/ispell-word-then-abbrev (p)
+  "Call `ispell-word'. Then create an abbrev for the correction made.
+With prefix P, create local abbrev. Otherwise it will be global."
+  (interactive "P")
+  (let ((bef (downcase (or (thing-at-point 'word) ""))) aft)
+    (call-interactively 'ispell-word)
+    (setq aft (downcase (or (thing-at-point 'word) "")))
+    (unless (string= aft bef)
+      (message "\"%s\" now expands to \"%s\" %sally"
+               bef aft (if p "loc" "glob"))
+      (define-abbrev
+        (if p local-abbrev-table global-abbrev-table)
+        bef aft))))
+
+(setq save-abbrevs t)
+(setq-default abbrev-mode t)
+
 (provide 'init-spelling)
