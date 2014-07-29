b72ab11b0b5111d06ba37b9fa233aee96b1f735e
Adam Frey <adam@adamfrey.me>
2014-07-29 00:22:16 -0400

    Get vertical ido mode

--- lisp/init-ido.el
+++ lisp/init-ido.el
@@ -1,7 +1,12 @@
 ;; Use C-f during file selection to switch to regular find-file
 (require 'ido)
+(require 'ido-vertical-mode)
+(require 'flx-ido)
 (ido-mode t)
 (ido-everywhere t)
+(ido-vertical-mode 1)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
 (setq ido-enable-flex-matching t)
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length 0)
