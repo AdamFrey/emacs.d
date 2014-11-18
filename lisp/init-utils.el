55342e66440070efc016ab34320dc460b6b3e364
Adam Frey <adam@adamfrey.me>
2014-11-18 13:27:07 -0500

    Add delete-file-and-buffer

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -53,6 +53,19 @@
     (delete-file (buffer-file-name))
     (kill-this-buffer)))
 
+(defun delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(global-set-key (kbd "C-c D")  'delete-file-and-buffer)
 
 ;;----------------------------------------------------------------------------
 ;; Rename the current file
