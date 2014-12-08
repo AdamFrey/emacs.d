17b9010ddccd36ada2ec939180b0484c84189fae
Adam Frey <adam@adamfrey.me>
2014-12-07 21:59:49 -0500

    Add missing "require-package" line for projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,5 +1,8 @@
+(require-package 'projectile)
+
 (projectile-global-mode)
-(provide 'init-projectile)
 (global-set-key (kbd "C-, f") 'projectile-find-file)
 (global-set-key (kbd "C-, a") 'ag-project-files)
 (global-set-key (kbd "C-, b") 'projectile-switch-to-buffer)
+
+(provide 'init-projectile)
