85b7072a85c6dfea86a698eadf9f4e0378339707
Adam Frey <adam@adamfrey.me>
2014-12-16 14:41:04 -0500

    Use M-o for switching windows

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -5,12 +5,8 @@
 
 
 
-;; Make "C-x o" prompt for a target window when there are more than 2
-(require-package 'switch-window)
-(require 'switch-window)
-(setq switch-window-shortcut-style 'alphabet)
-(global-set-key (kbd "C-x o") 'switch-window)
 
+(global-set-key (kbd "M-o") 'other-window)
 
 ;;----------------------------------------------------------------------------
 ;; When splitting window, show (other-buffer) in the new window
