745c588b5137c3fcacefde67fa0995364e96da9b
Adam Frey <adam@adamfrey.me>
2015-04-07 15:55:38 -0400

    Add kill-sexp-or-directory

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -380,8 +380,8 @@ With arg N, insert N newlines."
 ;; Indentation
 ;;-------------------------------------------
 
-;(require-package 'aggressive-indent)
-;(global-aggressive-indent-mode)
+;;(require-package 'aggressive-indent)
+;;(global-aggressive-indent-mode)
 
 ;;-------------------------------------------
 ;; Kill Whole lines
@@ -407,5 +407,49 @@ prefix argument."
 (global-set-key [remap paredit-kill] (bol-with-prefix paredit-kill))
 (global-set-key [remap org-kill-line] (bol-with-prefix org-kill-line))
 (global-set-key [remap kill-line] (bol-with-prefix paredit-kill))
+
+;;-------------------------------------------
+;; kill sexp or directory http://endlessparentheses.com/kill-sexp-or-directory.html?source=rss
+;;-------------------------------------------
+
+(defun endless/forward-kill-sexp-or-dir (&optional p)
+  "Kill forward sexp or directory.
+If inside a string or minibuffer, and if it looks like
+we're typing a directory name, kill forward until the next
+/. Otherwise, `kill-sexp'"
+  (interactive "p")
+  (if (< p 0)
+      (endless/backward-kill-sexp-or-dir (- p))
+    (let ((r (point)))
+      (if (and (or (in-string-p)
+                   (minibuffer-window-active-p
+                    (selected-window)))
+               (looking-at "[^[:blank:]\n\r]*[/\\\\]"))
+          (progn (search-forward-regexp
+                  "[/\\\\]" nil nil p)
+                 (kill-region r (point)))
+        (kill-sexp p)))))
+
+(defun endless/backward-kill-sexp-or-dir (&optional p)
+  "Kill backwards sexp or directory."
+  (interactive "p")
+  (if (< p 0)
+      (endless/forward-kill-sexp-or-dir (- p))
+    (let ((r (point))
+          (l (save-excursion
+               (point))))
+      (if (and (or (in-string-p)
+                   (minibuffer-window-active-p
+                    (selected-window)))
+               (looking-back "[/\\\\][^[:blank:]\n\r]*"))
+          (progn (backward-char)
+                 (search-backward-regexp
+                  "[/\\\\]" (point-min) nil p)
+                 (forward-char)
+                 (kill-region (point) l))
+        (kill-sexp (- p))))))
+
+(global-set-key (kbd "M-k") #'endless/forward-kill-sexp-or-dir)
+(global-set-key (kbd "C-M-k") #'endless/backward-kill-sexp-or-dir)
 
 (provide 'init-editing-utils)
