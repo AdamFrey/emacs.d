9a945c83de95e15845b4cfd57df50769144b12b4
Adam Frey <adam@adamfrey.me>
2015-05-14 21:23:59 -0400

    Change my default project search from ag to grep

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -2,7 +2,7 @@
 
 (projectile-global-mode)
 (global-set-key (kbd "C-, f") 'projectile-find-file)
-(global-set-key (kbd "C-, a") 'ag-project-files)
+(global-set-key (kbd "C-, a") 'projectile-grep)
 (global-set-key (kbd "C-, b") 'projectile-switch-to-buffer)
 
 (provide 'init-projectile)
