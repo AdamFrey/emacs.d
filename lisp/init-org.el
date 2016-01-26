31ea49d6c43897c2b8890625a7ca8d8a30233928
Adam Frey <adam@adamfrey.me>
2016-01-25 23:53:12 -0500

    Fix uses of C-, in org-mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -8,7 +8,8 @@
 
 (define-key global-map (kbd "C-c l") 'org-store-link)
 (define-key global-map (kbd "C-c a") 'org-agenda)
-(define-key org-mode-map (kbd "C-,") nil)
+(after-load 'org
+  (define-key org-mode-map (kbd "C-,") nil))
 
 ;; Various preferences
 (setq org-log-done t
