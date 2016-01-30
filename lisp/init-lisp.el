aa33a04a938d241d645dcf8f33037f377785f90d
Adam Frey <adam@adamfrey.me>
2016-01-30 09:55:55 -0500

    Bump up the garbage collection frequency

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -120,6 +120,11 @@
 (setq load-prefer-newer t)
 
 ;; ----------------------------------------------------------------------------
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; ----------------------------------------------------------------------------
 ;; Highlight current sexp
 ;; ----------------------------------------------------------------------------
 
