7cb7a74fbbe51e58048fcf2d51f1ce3d8349d3aa
Adam Frey <adam@adamfrey.me>
2016-04-29 10:23:30 -0400

    Add clojure-refactor

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -15,5 +15,18 @@
 
 (add-auto-mode 'clojure-mode "\\.boot\\'" "\\.pxi\\'")
 
+;;;;;;;;;;;;;;;;;;
+;; clj-refactor ;;
+;;;;;;;;;;;;;;;;;;
+
+(require-package 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+  (setq clojure-defun-style-default-indent t)
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
 (provide 'init-clojure)
