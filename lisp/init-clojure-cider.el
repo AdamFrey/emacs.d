0a7bb2339a5fb831d135630dabece987f9303395
Adam Frey <adam@adamfrey.me>
2016-06-23 12:09:11 -0400

    Update clojure-cider config

--- lisp/init-clojure-cider.el
+++ lisp/init-clojure-cider.el
@@ -10,30 +10,21 @@
 
     ;; nrepl isn't based on comint
     (add-hook 'cider-repl-mode-hook
-              (lambda () (setq show-trailing-whitespace nil))))
+              (lambda () (setq show-trailing-whitespace nil)))))
 
-(after-load 'cider
-  (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-  (add-hook 'cider-mode-hook 'ac-cider-setup)
-  (after-load 'auto-complete
-    (add-to-list 'ac-modes 'cider-mode)
-    (add-to-list 'ac-modes 'cider-repl-mode))
+(setq cider-repl-use-pretty-printing t
+      cider-repl-display-help-banner nil)
 
-  (add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-  (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
-  (add-hook 'cider-mode-hook 'eldoc-mode)
-  (add-hook 'cider-repl-mode-hook 'subword-mode)
-  (add-hook 'cider-repl-mode-hook 'paredit-mode)
-  (define-key cider-mode-map (kbd "C-c C-d") 'ac-cider-popup-doc)
-  (define-key cider-mode-map (kbd "C-c m") 'cider-macroexpand-1)
+;; (after-load 'cider
+;;   ;; (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+;;   ;; (add-hook 'cider-mode-hook 'ac-cider-setup)
+;;   ;; (after-load 'auto-complete
+;;   ;;   (add-to-list 'ac-modes 'cider-mode)
+;;   ;;   (add-to-list 'ac-modes 'cider-repl-mode))
 
-  ;; nrepl isn't based on comint
-  (add-hook 'cider-repl-mode-hook
-            (lambda () (setq show-trailing-whitespace nil))))
-
-;; (require-package 'flycheck-clojure)
-;; (after-load 'clojure-mode
-;;   (after-load 'flycheck
-;;     (flycheck-clojure-setup)))
+;;   ;; (add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;;   ;; (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+;;   ;; (define-key cider-mode-map (kbd "C-c C-d") 'ac-cider-popup-doc)
+;;   (define-key cider-mode-map (kbd "C-c m") 'cider-macroexpand-1)
 
 (provide 'init-clojure-cider)
