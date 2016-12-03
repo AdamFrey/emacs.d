e5a06e6d8bcf580cc3051834a2315f976b0b7cf4
Adam Frey <adam@adamfrey.me>
2016-12-03 13:29:24 -0600

    Add some clj-refactor settings

--- lisp/init-clojure.el
+++ lisp/init-clojure.el
@@ -33,10 +33,21 @@
 
 (with-eval-after-load 'clj-refactor
   (setq cljr-thread-all-but-last nil)
+  (setq cljr-auto-sort-ns t)
   (define-key clj-refactor-map "\C-ctf" #'cljr-thread-first-all)
   (define-key clj-refactor-map "\C-ctl" #'cljr-thread-last-all)
   (define-key clj-refactor-map "\C-cu" #'cljr-unwind)
-  (define-key clj-refactor-map "\C-cU" #'cljr-unwind-all))
+  (define-key clj-refactor-map "\C-cU" #'cljr-unwind-all)
+
+  (add-to-list 'cljr-magic-require-namespaces
+               '("d"  . "datomic.api"))
+  (add-to-list 'cljr-magic-require-namespaces
+               '("pp"  . "clojure.pprint"))
+
+  (setq cljr-favor-prefix-notation nil)
+  (setq cljr-favor-private-functions nil)
+  (setq cljr-clojure-test-declaration
+        "[clojure.test :refer :all]"))
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
 
